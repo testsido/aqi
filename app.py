520b21d6c12976049ea8058eebec9ace9df88777
testsido <62465411+testsido@users.noreply.github.com>
2020-05-20 19:02:50 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -39,6 +39,12 @@ def ANN():
 def Lasso():
     return render_template('Lasso.html')
 
+# For Rendering Results 
+
+@app.route('/PredictionSuccess')
+def PLinear():
+    return render_template('PLinear.html')
+
 
 @app.route('/predictLinear',methods=['POST'])
 def predictLinear():
@@ -64,7 +70,16 @@ def predictLinear():
         output=output/3.55;
     output = round(output, 2)
     # return json.dumps({'AQi':output});
-    return render_template('Linear.html', prediction_text='PM2.5 = {}'.format(output))
+    return render_template('PLinear.html', prediction_text='PM2.5 = {} µg/m3'.format(output),
+    temp='Temperature = {} °C'.format(int_features[0]),
+    maxtemp='Maximum Temperature = {} °C'.format(int_features[1]),
+    mintemp='Minimum Temperature = {} °C'.format(int_features[2]),
+    atm='Atmospheric Pressure = {} mm Hg'.format(int_features[3]),
+    hum='Humidity = {} %'.format(int_features[4]),
+    visi='Visibility = {} km'.format(int_features[5]),
+    ws='Wind Speed = {} m/s'.format(int_features[6]),
+    mws='Maximum Wind Speed = {} m/s'.format(int_features[7]))
+
 
 @app.route('/predictANN',methods=['POST'])
 def predictANN():
@@ -87,7 +102,7 @@ def predictANN():
 
     output = round(prediction[0], 2)
 
-    return render_template('ANN.html', prediction_text='PM2.5 = {}'.format(output))
+    return render_template('ANN.html', prediction_text='PM2.5 = {} µg/m3'.format(output))
 
 
 @app.route('/predictKNN',methods=['POST'])
@@ -111,7 +126,7 @@ def predictKNN():
 
     output = round(prediction[0], 2)
 
-    return render_template('KNN.html', prediction_text='PM2.5 = {}'.format(output))
+    return render_template('KNN.html', prediction_text='PM2.5 = {} µg/m3'.format(output))
 
 
 @app.route('/predictDecision',methods=['POST'])
@@ -135,7 +150,7 @@ def predictDecision():
 
     output = round(prediction[0], 2)
 
-    return render_template('Decision.html', prediction_text='PM2.5 = {}'.format(output))
+    return render_template('Decision.html', prediction_text='PM2.5 = {} µg/m3'.format(output))
 
 @app.route('/predictRF',methods=['POST'])
 def predictRF():
@@ -158,7 +173,7 @@ def predictRF():
 
     output = round(prediction[0], 2)
 
-    return render_template('RForest.html', prediction_text='PM2.5 = {}'.format(output))
+    return render_template('RForest.html', prediction_text='PM2.5 = {} µg/m3'.format(output))
 
 @app.route('/predictLasso',methods=['POST'])
 def predictLasso():
@@ -181,7 +196,15 @@ def predictLasso():
 
     output = round(prediction[0], 2)
 
-    return render_template('Lasso.html', prediction_text='PM2.5 = {}'.format(output))
+    return render_template('Lasso.html', prediction_text='PM2.5 = {} µg/m3'.format(output),
+    temp='Temperature = {} °C'.format(int_features[0]),
+    maxtemp='Maximum Temperature = {} °C'.format(final_features[1]),
+    mintemp='Minimum Temperature = {} °C'.format(final_features[2]),
+    atm='Atmospheric Pressure = {} mm Hg'.format(final_features[3]),
+    hum='Humidity = {} %'.format(final_features[4]),
+    visi='Visibility = {} km'.format(final_features[5]),
+    ws='Wind Speed = {} m/s'.format(final_features[6]),
+    mws='Maximum Wind Speed = {} m/s'.format(final_features[7]))
 
 # @app.route('/predict_api',methods=['POST'])
 # def predict_api():
